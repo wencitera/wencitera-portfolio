20507877d207ce72f42d67484714a01be804fb45
Wenceslao Ariel Citera <wencitera@gmail.com>
2021-09-15 21:11:37 -0300

    better responsive navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,30 @@
 import React from 'react'
-import { Button, makeStyles } from '@material-ui/core';
+import { AppBar, IconButton, makeStyles } from '@material-ui/core';
 import AccountTreeIcon from '@material-ui/icons/AccountTree';
 import WorkIcon from '@material-ui/icons/Work';
 import MailIcon from '@material-ui/icons/Mail';
 import HomeIcon from '@material-ui/icons/Home';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import BookIcon from '@material-ui/icons/Book';
+
 const useStyles = makeStyles((theme) => ({
-    navbar_nav: {
-        position: "fixed",
-        backgroundColor: theme.palette.primary.dark,
-        overflow: "scroll",
-        padding: 0,
-        margin: 0,
+    root: {
+        width: 500,
+    },
+    appBar: {
+        top: 'auto',
+        bottom: 0,
+        flexDirection: "row",
         alignItems: "center",
         justifyContent: "center",
-        '&::-webkit-scrollbar': {
-            width: "0%",
-        },
-        [theme.breakpoints.down("md")]: {
-            position: 'relative',
-            flexDirection: "row",
-            width: "100%",
-            bottom: '0',
-            display: "flex",
-        },
+        boxShadow: 'none',
+        backgroundColor: theme.palette.primary.dark,
         [theme.breakpoints.up("md")]: {
             display: "flex",
             flexDirection: "column",
-            height: "100%",
+            height: "100vh",
+            width: 50,
+            left: 0
         }
     },
     button: {
@@ -36,30 +32,37 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
+
 export default function NavBar() {
     const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
     return (
-        <div className={`${classes.navbar}`}>
-            <div className={classes.navbar_nav}>
-                <Button href='/' className={classes.button}>
-                    <HomeIcon fontSize="large" />
-                </Button>
-                <Button href='/sobremi' className={classes.button}>
-                    <AccountBoxIcon fontSize="large" />
-                </Button>
-                <Button href='/conocimientos' className={classes.button}>
-                    <BookIcon fontSize="large" />
-                </Button>
-                <Button href='/proyectos' className={classes.button}>
-                    <AccountTreeIcon fontSize="large" />
-                </Button>
-                <Button href='/experiencia' className={classes.button}>
-                    <WorkIcon fontSize="large" />
-                </Button>
-                <Button href='/contacto' className={classes.button}>
-                    <MailIcon fontSize="large" />
-                </Button>
-            </div>
-        </div>
+        <AppBar
+            value={value}
+            onChange={(event, newValue) => {
+                setValue(newValue);
+            }}
+            className={classes.appBar}
+        >
+            <IconButton href='/' label="Inicio" className={classes.button}>
+                <HomeIcon fontSize="large" />
+            </IconButton>
+            <IconButton href='/sobremi' className={classes.button}>
+                <AccountBoxIcon fontSize="large" />
+            </IconButton>
+            <IconButton href='/conocimientos' className={classes.button}>
+                <BookIcon fontSize="large" />
+            </IconButton>
+            <IconButton href='/proyectos' className={classes.button}>
+                <AccountTreeIcon fontSize="large" />
+            </IconButton>
+            <IconButton href='/experiencia' className={classes.button}>
+                <WorkIcon fontSize="large" />
+            </IconButton>
+            <IconButton href='/contacto' className={classes.button}>
+                <MailIcon fontSize="large" />
+            </IconButton>      
+        </AppBar>
     )
 }
