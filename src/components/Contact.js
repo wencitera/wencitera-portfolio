c1e65f7d50e16875f3fe3ade7dd3b1ec3b6f002f
Wenceslao Ariel Citera <wencitera@gmail.com>
2021-09-20 20:19:05 -0300

    Contact me listo, agregada otra fuente

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,49 @@
-import { Grid, makeStyles } from '@material-ui/core';
+import { Grid, makeStyles, Typography } from '@material-ui/core';
 import React from 'react'
 import { SiCodewars, SiDiscord, SiGithub, SiGmail, SiLinkedin } from 'react-icons/si';
+import ProfileAvatar from '../assets/ProfileAvatar.jpg'
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1,
-        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: "90vh",
+        width: "auto",
         [theme.breakpoints.up('sm')]: {
             marginLeft: "5rem",
         },
     },
+    list: {
+        marginTop: "10px",
+        display: 'flex',
+        flexDirection: 'row',
+        listStyle: 'none',
+        margin: 0,
+    },
     center: {
         textAlign: "center",
+    },
+    title_container:{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "flex-end",
+    },
+    title: {
+        fontFamily: 'Quicksand, sans-serif',
+        color: theme.palette.secondary.main,
     },
     icon: {
         fontSize: "3rem",
         color: theme.palette.secondary.main,
-        textDecoration: "none"
+        textDecoration: "none",
+        margin: "5px",
     },
     image: {
-        padding: '2vw',
-        width: "auto",
-        maxHeight: "60rem",
+        marginTop: "10px",
         borderRadius: "50% 50%",
+        display: "block",
+        height: "15rem",
+        width: "auto",
     }
 }))
 
@@ -57,14 +78,20 @@ const socialMedia = [
 export default function Contact() {
     const classes = useStyles();
     return (
-        <div className={classes.root}>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
+        <Grid container direction="column" className={classes.root} justifyContent='center' alignItems="center">
+            <Grid item xs className={classes.title_container}>
+                <Typography className={classes.title} variant="h2">Contáctame</Typography>
+            </Grid>
+            <Grid item xs>
+                <img className={classes.image} src={ProfileAvatar} alt="Profile avatar" />
+            </Grid>
+            <Grid item xs className={classes.list}>
                 {socialMedia.map((item, index) => (
-                    <Grid key={index} item xs className={classes.center}>
+                    <li key={index} item xs className={classes.center}>
                         <a className={classes.icon} href={item.url} target="_blank" rel="noreferrer">{item.logo}</a>
-                    </Grid>
+                    </li>
                 ))}
             </Grid>
-        </div>
+        </Grid>
     )
 }
