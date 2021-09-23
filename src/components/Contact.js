5bbacbb624b0b5eff5d92a0761377ecf675f157f
Wenceslao Ariel Citera <wencitera@gmail.com>
2021-09-23 19:37:47 -0300

    Solucionado contact js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import { Grid, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import React from 'react'
 import { SiCodewars, SiDiscord, SiGithub, SiGmail, SiLinkedin } from 'react-icons/si';
@@ -8,11 +8,17 @@ const useStyles = makeStyles((theme) => ({
     root: {
         alignItems: 'center',
         justifyContent: 'center',
-        height: "90vh",
+        height: "100vh",
         width: "auto",
         [theme.breakpoints.up('sm')]: {
             marginLeft: "5rem",
         },
+    },
+    center: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column'
     },
     list: {
         marginTop: "10px",
@@ -20,14 +26,6 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: 'row',
         listStyle: 'none',
         margin: 0,
-    },
-    center: {
-        textAlign: "center",
-    },
-    title_container:{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "flex-end",
     },
     title: {
         fontFamily: 'Quicksand, sans-serif',
@@ -40,12 +38,10 @@ const useStyles = makeStyles((theme) => ({
         margin: "5px",
     },
     image: {
-        marginTop: "10px",
         borderRadius: "50% 50%",
-        display: "block",
         height: "15rem",
         width: "auto",
-    }
+    },
 }))
 
 const socialMedia = [
@@ -79,20 +75,16 @@ const socialMedia = [
 export default function Contact() {
     const classes = useStyles();
     return (
-        <Grid container direction="column" className={classes.root} justifyContent='center' alignItems="center">
-            <Grid item xs className={classes.title_container}>
-                <Typography className={classes.title} variant="h2">Redes sociales</Typography>
-            </Grid>
-            <Grid item xs>
-                <img className={classes.image} src={ProfileAvatar} alt="Profile avatar" />
-            </Grid>
-            <Grid item xs className={classes.list}>
+        <div className={`${classes.root} ${classes.center}`}>
+            <Typography className={classes.title} variant="h2">Redes Sociales</Typography>
+            <img className={classes.image} src={ProfileAvatar} alt="Profile avatar" />
+            <div className={classes.list}>
                 {socialMedia.map((item, index) => (
                     <li key={index} item xs className={classes.center}>
                         <a className={classes.icon} href={item.url} target="_blank" rel="noreferrer">{item.logo}</a>
                     </li>
                 ))}
-            </Grid>
-        </Grid>
+            </div>
+        </div>
     )
 }
